1d49686eef8bf4a7d30c1e0075bccdf32721845b
bataevabdullah2009-pixel <bataevabdullah2009@gmail.com>
2026-08-08 14:38:21 +0300

    feat: add compatibility and dimension validation

---
+++ packages/pricing/src/index.ts
@@ -0,0 +1,17 @@
+export { filterCompatibleProfiles, validatePricingSelection } from './validation.js';
+export {
+  calculationStatuses,
+  type AppliedPriceOverride,
+  type AppliedPriceRule,
+  type CalculationStatus,
+  type ConfiguratorOption,
+  type PricingFixture,
+  type PricingParityStatus,
+  type PricingRuleKind,
+  type PricingRuleOptions,
+  type PricingRuleProfile,
+  type PricingSelection,
+  type PricingValidationDetail,
+  type PricingValidationResult,
+  type PricingVerificationStatus,
+} from './types.js';
